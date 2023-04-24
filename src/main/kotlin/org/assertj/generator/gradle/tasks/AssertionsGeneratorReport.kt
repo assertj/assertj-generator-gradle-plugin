73a391b88e3fd6fa3f649ecb9516d3cdb447fc1e
Kevin Brightwell <kevin.brightwell2@gmail.com>
2023-04-24 15:55:11 -0400

    [CI] Fix-up local tasks broken by CI checks (#50)
    Also adds support for dependency linting.

--- src/main/kotlin/org/assertj/generator/gradle/tasks/AssertionsGeneratorReport.kt
+++ src/main/kotlin/org/assertj/generator/gradle/tasks/AssertionsGeneratorReport.kt
@@ -10,7 +10,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.assertj.generator.gradle.internal.tasks
+package org.assertj.generator.gradle.tasks
 
 import com.google.common.reflect.TypeToken
 import org.assertj.assertions.generator.AssertionsEntryPointType
