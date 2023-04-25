6cac79c32004be2f3439bbb9818b738af72c64aa
Kevin Brightwell <kevin.brightwell2@gmail.com>
2023-04-25 10:58:05 -0400

    add license

--- src/main/kotlin/org/assertj/generator/gradle/tasks/config/patterns/JavaIdentifierNamePatternFilterableBase.kt
+++ src/main/kotlin/org/assertj/generator/gradle/tasks/config/patterns/JavaIdentifierNamePatternFilterableBase.kt
@@ -1,3 +1,15 @@
+/*
+ * Copyright 2023. assertj-generator-gradle-plugin contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package org.assertj.generator.gradle.tasks.config.patterns
 
 import java.io.IOException
@@ -16,7 +28,7 @@ sealed class JavaIdentifierNamePatternFilterableBase<T, SELF>
   internal fun asPredicate(): Predicate<T> {
     return Predicate { t ->
       (includePredicates.isEmpty() || includePredicates.any { it.test(t) }) &&
-        (excludePredicates.isEmpty() || excludePredicates.none { it.test(t) })
+          (excludePredicates.isEmpty() || excludePredicates.none { it.test(t) })
     }
   }
 
