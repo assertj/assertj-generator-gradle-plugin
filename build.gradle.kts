a632af499c748258a1cb6fb4e14c20663e0d4645
Kevin Brightwell <kevin.brightwell2@gmail.com>
2023-08-17 06:48:36 -0400

    add back -api dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -72,6 +72,7 @@ dependencies {
 
   val kotlinVersion = "1.6.21"
   implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+  implementation("org.jetbrains.kotlin:kotlin-gradle-plugin-api:$kotlinVersion")
 
   testCompileOnly("org.jetbrains:annotations:24.0.1")
 
