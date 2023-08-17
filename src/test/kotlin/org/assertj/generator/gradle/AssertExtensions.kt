759a6c77241f01c3446be27091e36a548fa38397
Kevin Brightwell <kevin.brightwell2@gmail.com>
2023-08-17 06:40:12 -0400

    add tests

--- src/test/kotlin/org/assertj/generator/gradle/AssertExtensions.kt
+++ src/test/kotlin/org/assertj/generator/gradle/AssertExtensions.kt
@@ -3,11 +3,22 @@ package org.assertj.generator.gradle
 import org.assertj.core.api.AbstractObjectAssert
 import org.gradle.testkit.runner.BuildTask
 import org.gradle.testkit.runner.TaskOutcome
+import org.gradle.testkit.runner.TaskOutcome.FROM_CACHE
+import org.gradle.testkit.runner.TaskOutcome.SUCCESS
+import org.gradle.testkit.runner.TaskOutcome.UP_TO_DATE
 
-internal fun <SELF : AbstractObjectAssert<SELF, BuildTask?>> SELF.isSuccessful(): SELF = isOutcome(TaskOutcome.SUCCESS)
-internal fun <SELF : AbstractObjectAssert<SELF, BuildTask?>> SELF.isUpToDate(): SELF = isOutcome(TaskOutcome.UP_TO_DATE)
+internal fun <SELF : AbstractObjectAssert<SELF, BuildTask?>> SELF.isSuccessful(): SELF = isOutcome(SUCCESS)
 
-private fun <SELF : AbstractObjectAssert<SELF, BuildTask?>> SELF.isOutcome(outcome: TaskOutcome): SELF {
-  extracting { it?.outcome }.`as` { "task.outcome == $outcome" }.isEqualTo(outcome)
+internal fun <SELF : AbstractObjectAssert<SELF, BuildTask?>> SELF.isSuccessOrCached(): SELF = isOutcome(
+  SUCCESS,
+  FROM_CACHE
+)
+
+internal fun <SELF : AbstractObjectAssert<SELF, BuildTask?>> SELF.isUpToDate(): SELF = isOutcome(UP_TO_DATE)
+
+private fun <SELF : AbstractObjectAssert<SELF, BuildTask?>> SELF.isOutcome(vararg outcomes: TaskOutcome): SELF {
+  extracting { it?.outcome }
+    .`as` { "task.outcome in { ${outcomes.joinToString(", ")} }" }
+    .isIn(outcomes.toSet())
   return this
 }
